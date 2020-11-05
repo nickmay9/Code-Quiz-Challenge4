9b779441ecb039beea5690a7e49dceef5873bb78
Nick May <nickmay9@outlook.com>
2020-11-05 17:41:45 -0500

    figured out how to hide section on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
 //Global Variables
 var pageContentEl = document.querySelector("#page-content");
-
-
+var quizObjArr = []; 
+var quizObj = {
+    question: "What is 10 + 2?",
+    optionA: "102",
+    optionB: "10",
+    optionC: "12",
+    optionD: "2"
+};
 
 function startQuizHandler() {
+    var startScreen = document.getElementById("quiz-start");
+    startScreen.hidden = "true";
+
     //create div item to hold quiz question and multiple choice answers
     var divItemEl = document.createElement("div");
     divItemEl.className = "quiz-question-container";
@@ -16,7 +25,7 @@ function startQuizHandler() {
 
     var quizAnswersEl = document.createElement("ul");
     quizAnswersEl.className = "quiz-answers";
-    quizAnswersEl.innerHTML = "<li>102</li><li>10</li><li>12</li><li>2</li>";
+    quizAnswersEl.innerHTML = "<button type='submit'>1. 102</button><button type='submit'>2. 10</button><button type='submit'>3. 12</button><button type='submit'>4. 2</button>";
 
     divItemEl.appendChild(quizAnswersEl);
 
